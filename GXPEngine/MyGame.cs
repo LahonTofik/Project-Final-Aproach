70b85846698ed6400523812397a595c13d192da6
R-Bleach <loljk366@gmail.com>
2024-05-06 14:13:26 +0200

    physics added

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -1,29 +1,104 @@
 using System;                                   // System contains a lot of default C# libraries 
 using GXPEngine;                                // GXPEngine contains the engine
 using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using System.Collections.Generic;
+using System.Threading;
 
 public class MyGame : Game {
-	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
-	{
-		// Draw some things on a canvas:
-		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.MediumPurple);
-		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(width / 2, height / 2, 200, 200);
-		canvas.Fill(50);
-		canvas.TextSize(32);
-		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Welcome!", width / 2, height / 2);
+    bool _stepped = false;
+    bool _paused = false;
+    int _stepIndex = 0;
+    int _startSceneNumber = 0;
 
-		// Add the canvas to the engine to display it:
-		AddChild(canvas);
-		Console.WriteLine("MyGame initialized");
-	}
+    Canvas _lineContainer = null;
 
-	// For every game object, Update is called every frame, by the engine:
-	void Update() {
-		// Empty
-	}
+    List<Ball> _movers;
+    List<LineSegment> _lines;
+    EasyDraw congrats;
+	Font rowdies;
+
+    public int GetNumberOfLines()
+    {
+        return _lines.Count;
+    }
+
+    public LineSegment GetLine(int index)
+    {
+        if (index >= 0 && index < _lines.Count)
+        {
+            return _lines[index];
+        }
+        return null;
+    }
+
+    public int GetNumberOfMovers()
+    {
+        return _movers.Count;
+    }
+
+    public Ball GetMover(int index)
+    {
+        if (index >= 0 && index < _movers.Count)
+        {
+            return _movers[index];
+        }
+        return null;
+    }
+    public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
+    {
+
+        _movers = new List<Ball>();
+        _lines = new List<LineSegment>();
+        // Draw some things on a canvas:
+        EasyDraw canvas = new EasyDraw(800, 600);
+        canvas.Clear(Color.MediumPurple);
+        canvas.Fill(Color.Yellow);
+        canvas.Ellipse(width / 2, height / 2, 200, 200);
+        canvas.Fill(50);
+        canvas.TextSize(32);
+        canvas.TextAlign(CenterMode.Center, CenterMode.Center);
+        canvas.Text("Welcome!", width / 2, height / 2);
+
+        // Add the canvas to the engine to display it:
+        AddChild(canvas);
+        Console.WriteLine("MyGame initialized");
+
+    }
+    void StepThroughMovers()
+    {
+        if (_stepped)
+        { // move everything step-by-step: in one frame, only one mover moves
+            _stepIndex++;
+            if (_stepIndex >= _movers.Count)
+            {
+                _stepIndex = 0;
+            }
+            if (_movers[_stepIndex].moving)
+            {
+                _movers[_stepIndex].Step();
+            }
+        }
+        else
+        { // move all movers every frame
+            for (int i = _movers.Count - 1; i >= 0; i--)
+            {
+                if (_movers[i].moving)
+                {
+                    _movers[i].Step();
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!_paused)
+        {
+            StepThroughMovers();
+        }
+    }
+
+    // For every game object, Update is called every frame, by the engine:
 
 	static void Main()                          // Main() is the first method that's called when the program is run
 	{
---
+++ GXPEngine/PlayerBall.cs
@@ -0,0 +1,60 @@
+﻿using GXPEngine;
+using GXPEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PlayerBall : Ball
+{
+    float speed = 3;
+    float maxSpeed = 6;
+    float jumpPow = 15;
+    public PlayerBall(int pRadius, Vec2 pPosition, Vec2 pVelocity = default, Vec2 pGravity = default, bool moving = true, bool pIsPlayer = false) : base(pRadius, pPosition, pVelocity, pGravity, moving, pIsPlayer)
+    {
+    }
+    void Update()
+    {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        if (Input.GetKey(Key.A))
+        {
+            if (velocity.x >= -maxSpeed)
+            {
+                velocity.x -= speed;
+            }
+            if (velocity.x <= -maxSpeed)
+            {
+                velocity.x = -maxSpeed;
+            }
+            movingNow = true;
+        }
+        else if (Input.GetKey(Key.D)) ;
+        else
+        {
+            movingNow = false;
+        }
+        if (Input.GetKey(Key.D))
+        {
+            if (velocity.x <= maxSpeed)
+            {
+                velocity.x += speed;
+            }
+            if (velocity.x >= maxSpeed)
+            {
+                velocity.x = maxSpeed;
+            }
+            movingNow = true;
+        }
+        else if (Input.GetKey(Key.A)) ;
+        else movingNow = false;
+        if (Input.GetKeyDown(Key.SPACE) && jump > 0)
+        {
+            velocity.y -= jumpPow;
+            jump -= 1;
+        }
+    }
+}
