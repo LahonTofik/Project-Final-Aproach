0d7516edc067cdb8057dae737cf42e1c4c6529ec
LuckyPotatoes <lahon.live@gmail.com>
2024-05-06 14:53:53 +0200

    Added Tiled Level Loading

---
+++ GXPEngine/Level.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiledMapParser;
+using GXPEngine;
+
+
+public class Level : GameObject
+{
+    readonly TiledLoader loader;
+    public Level(string filename)
+    {
+        loader = new TiledLoader(filename);
+        CreateLevel();
+    }
+        void CreateLevel(bool IncludeImageLayer = true)
+    {
+        loader.autoInstance = true;
+        loader.rootObject = this;
+
+        loader.addColliders = false;
+        loader.LoadImageLayers();
+
+        loader.LoadTileLayers(0); // background (skybox/ scenery)
+        loader.LoadTileLayers(1); // background and props
+        loader.addColliders = true;
+        loader.LoadTileLayers(2); // platforms and walls (everything that is collidable)
+        loader.LoadObjectGroups();
+
+    }
+}
--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -1,23 +1,49 @@
 using System;                                   // System contains a lot of default C# libraries 
 using GXPEngine;                                // GXPEngine contains the engine
+using System.Collections.Generic;
 using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
 
 public class MyGame : Game {
-	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
-	{
-		// Draw some things on a canvas:
-		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.MediumPurple);
-		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(width / 2, height / 2, 200, 200);
-		canvas.Fill(50);
-		canvas.TextSize(32);
-		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Welcome!", width / 2, height / 2);
+	public int currentLevel = 0; // levels start from level 0
+	public string[] levels = new string[1];	// amount of levels is 1
 
-		// Add the canvas to the engine to display it:
-		AddChild(canvas);
-		Console.WriteLine("MyGame initialized");
+	public bool nextLevel = false;
+
+	public int GetCurrentLevel()
+	{
+		return currentLevel;
+	}
+	public void SetCurrentLevel(int _value)
+	{
+		currentLevel = _value;
+		if (currentLevel >= 4) { currentLevel = 0; }
+		LoadLevel(levels[currentLevel]);
+	}
+
+	public MyGame() : base(800, 600, false)     
+	{
+		levels[0] = "TestLevel.tmx";
+
+		LoadLevel(levels[0]);
+
+	}
+
+	void DestroyAllLevels()
+	{
+		List<GameObject> children = GetChildren();
+		foreach (GameObject child in children)
+		{
+			child.Destroy();
+		}
+	}
+
+	private void LoadLevel(string name)
+	{
+		nextLevel = false;
+		DestroyAllLevels();
+		Level level = new Level(name);
+		AddChild(level);
+		//player = FindObjectOfType<Player>(); /*for when we add a player*/
 	}
 
 	// For every game object, Update is called every frame, by the engine:
@@ -25,8 +51,8 @@ public class MyGame : Game {
 		// Empty
 	}
 
-	static void Main()                          // Main() is the first method that's called when the program is run
+	static void Main()                         
 	{
-		new MyGame().Start();                   // Create a "MyGame" and start it
+		new MyGame().Start();                   
 	}
 }
