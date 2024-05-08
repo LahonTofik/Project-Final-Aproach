06cde88c1ee2750e62165ae87809a8cd074aa401
R-Bleach <loljk366@gmail.com>
2024-05-08 11:49:47 +0200

    level scrolling

--- GXPEngine/Level.cs
+++ GXPEngine/Level.cs
@@ -13,6 +13,7 @@ using GXPEngine.OpenGL;
 public class Level : GameObject
 {
     MyGame myGame;
+    private PlayerBall player;
     readonly TiledLoader loader;
     public Level(string filename)
     {
@@ -32,28 +33,65 @@ public class Level : GameObject
         loader.addColliders = true;
         loader.LoadTileLayers(2); // platforms and walls (everything that is collidable)
         loader.LoadObjectGroups();
+        CreateCollisions();
     }
     void CreateCollisions()
     {
-        CollisionLine[] lines = FindObjectOfType<CollisionLine>();
+        CollisionLine[] lines = FindObjectsOfType<CollisionLine>();
         foreach (var line in lines)
         {
             CollisionLine cLine = line as CollisionLine;
             var corners = cLine.GetExtents();
 
-            for (int i = 0; 0 < corners; i++)
+            for (int i = 0; 0 <= corners.Length; i++)
             {
-                if (i != corners)
+                if (i != corners.Length)
                 {
                     // top line
                     myGame.AddChild(new LineSegment(new Vec2(corners[i].x, corners[i].y), new Vec2(corners[i + 1].x, corners[i + 1].y)));
                 }
                 else
                 {
-                    MyGame.AddChild(new LineSegment(new Vec2(corners[i].x, corners[i].y), new Vec2(corners[0].x, corners[0].y)));
+                    myGame.AddChild(new LineSegment(new Vec2(corners[i].x, corners[i].y), new Vec2(corners[0].x, corners[0].y)));
                 }
             }
         }
 
+    }
+    void HandleScroll()
+    {
+        if (player == null) return;
+        int boundarySizex = 300;
+        int boundarySizey = 200;
+        if (player.x + x < boundarySizex)
+        {
+            x = boundarySizex - player.x;
+        }
+        if (player.x + x > game.width - boundarySizex)
+        {
+            x = game.width - boundarySizex - player.x;
+        }
+        if (player.y + y < boundarySizey)
+        {
+            y = boundarySizey - player.y;
+        }
+        if (player.y + y > game.height - boundarySizey)
+        {
+            y = game.height - boundarySizey - player.y;
+        }
+    }
+    void GameBoundary()
+    {
+        if (player == null) return;
+        if (x > 0) x = 0;
+        if (-x >= game.width * 2) x = (game.width * -2);
+        if (-y >= game.height) y = (game.height) * -1;
+        if (y > 0) y = 0;
+    }
+    void Update()
+    {
+        HandleScroll();
+        GameBoundary();
+        Console.WriteLine(game.height);
     }
 }
